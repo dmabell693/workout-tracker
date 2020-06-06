a4932d68ed234a436d7df8b8c0f4849bf6b23a2a
Daniel Abell <da450@cairn.edu>
2020-06-06 13:48:21 -0400

    added crud api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,4 +6,40 @@ module.exports = function(app) {
             res.json(dbWorkouts);
         });
     });
+
+    app.get("/api/workouts/range", function(req, res) {
+        db.Workout.find({}).then(function(dbWorkouts) {
+            res.json(dbWorkouts);
+        });
+    });
+
+    app.put("/api/workouts/:id", function(req, res) {
+        console.log(req.body);
+        db.Workout.updateOne({ _id: req.params.id }, { body: req.body }).then(function(dbWorkouts) {
+            console.log(dbWorkouts);
+          res.json(dbWorkouts);
+        });
+      });
+
+    app.post("/api/workouts", function(req, res) {
+        console.log(req.body);
+        db.Workout.create(req.body, function(error, data) {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(data);
+            }
+        });
+    });
 }
+
+// db.Workout.deleteMany({})
+//   .then(() => db.Workout.collection.insertMany(workoutSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
