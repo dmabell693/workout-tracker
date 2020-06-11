2b56b0f76d907dbd317e5555abfa31ee20f6202a
Daniel Abell <da450@cairn.edu>
2020-06-11 18:04:42 -0400

    removed unnecessary code; inserted explanatory comments

--- models/workout.js
+++ models/workout.js
@@ -1,25 +1,13 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
+// create schema to be exported
 let workoutSchema = new Schema({
     day: {type: Date, default: Date.now},
+    // setting exercises type to "Array" worked better than manually creating each possibility for exercises
     exercises: {type: Array, required: false},
-    // [
-    //     {
-    //     type: {type: String, required: true},
-    //     name: {type: String, required: true},
-    //     duration: {type: Number, required: true},
-    //     weight: {type: Number, required: false},
-    //     reps: {type: Number, required: false},
-    //     sets: {type: Number, required: false},
-    //     distance: {type: Number, required: false}
-    //     }
-    // ],
+    // set totalDuration to default value of 0 so it can be manipulated in apiRoutes
     totalDuration: {type: Number, required: false, default: 0}
-    // totalWeight: {type: Number, required: false, default: 0},
-    // totalSets: {type: Number, required: false, default: 0},
-    // totalReps: {type: Number, required: false, default: 0},
-    // totalDistance: {type: Number, required: false, default: 0}
 });
 
 let Workout = mongoose.model("Workout", workoutSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,75 +1,45 @@
 const db = require("../models");
 
 module.exports = function(app) {
+    // api route to get all workouts
     app.get("/api/workouts", function(req, res) {
+        // return all indexes in the "workouts" collection
         db.Workout.find({}).then(function(dbWorkouts) {
-
-            // let lastWorkout = dbWorkouts[(dbWorkouts.length - 1)];
-            // console.log(lastWorkout);
-
-            // let totalDuration = 0;
-            // for (i=0; i<lastWorkout.exercises.length; i++) {
-            //     totalDuration += lastWorkout.exercises[i].duration;
-            //     console.log(totalDuration);
-            // }
-
-            // lastWorkout.totalDuration = totalDuration;
-            // console.log(lastWorkout);
-
+            // filter through each workout
             dbWorkouts.forEach(workout => {
                 let totalDuration = 0;
-                // let totalWeight = 0;
-                // let totalSets = 0;
-                // let totalReps = 0;
-                // let totalDistance = 0;
+                // filter through each exercise in each workout to find each exercise duration
                 workout.exercises.forEach(exercise => {
-                  //calculate total duration
+                  //return total duration for each workout
                   totalDuration += exercise.duration;
                   workout.totalDuration = totalDuration;
-
-                //   //calculate total weight
-                //   totalWeight += (exercise.weight * exercise.sets * exercise.reps);
-                //   workout.totalWeight = totalWeight;
-
-                //   //calculate total sets
-                //   totalSets += exercise.sets;
-                //   workout.totalSets = totalSets;
-
-                //   //calculate total reps
-                //   totalReps += (exercise.sets * exercise.reps);
-                //   workout.totalReps = totalReps;
-
-                //   //calculate total distance
-                //   totalDistance += exercise.distance;
-                //   workout.totalDistance = totalDistance;
                 });
               });
-
-            console.log(dbWorkouts);
+            // send workouts with updated (but not saved) totalDuration times to server
             res.json(dbWorkouts);
         });
     });
 
     app.get("/api/workouts/range", function(req, res) {
+        // get all workouts
         db.Workout.find({}).then(function(dbWorkouts) {
             res.json(dbWorkouts);
-            console.log(dbWorkouts);
         });
     });
 
     app.put("/api/workouts/:id", function(req, res) {
-        console.log(req.body);
+        // find and update index using param id
         db.Workout.updateOne(
             { _id: req.params.id }, 
+            // update exercises array in the identified index
             { $push: { exercises: req.body } }
             ).then(function(dbWorkouts) {
-            console.log(dbWorkouts);
           res.json(dbWorkouts);
         });
       });
 
     app.post("/api/workouts", function(req, res) {
-        console.log(req.body);
+        // create new index (workout) in the collection (workouts)
         db.Workout.create(req.body, function(error, data) {
             if (error) {
                 res.send(error);
@@ -80,14 +50,3 @@ module.exports = function(app) {
         });
     });
 }
-
-// db.Workout.deleteMany({})
-//   .then(() => db.Workout.collection.insertMany(workoutSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
